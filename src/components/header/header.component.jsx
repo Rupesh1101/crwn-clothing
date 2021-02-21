5d2dd769d7460d2d817354df069e0d8e8cc37fb3
Rupesh1101 <rupeshjha7000@gmail.com>
2021-02-21 18:04:23 +0530

    Cart Icon implemented

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import {connect} from 'react-redux';
 import './header.styles.scss';
 import {Link} from 'react-router-dom';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import {auth} from '../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
 
 const Header = ({currentUser}) => (
     <div className='header'>
@@ -17,8 +19,13 @@ const Header = ({currentUser}) => (
                 <div className='options' onClick={() => auth.signOut()}>SIGN OUT</div>
                 : <Link className='options' to='/signin'>SIGN IN</Link>   
             }
+            <CartIcon />
         </div>
     </div>
 )
 
-export default Header;
+const mapStateToProps = state =>({
+    currentUser: state.user.currentUser
+})
+
+export default connect(mapStateToProps)(Header);
